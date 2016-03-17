2fbe2c4e1a81eeb1f4d156a6cd74e93735de0ba5
theflakes <theflakes@gmail.com>
2016-03-17 14:55:00 -0400

    Update elsa_query.sh

--- elsa_query.sh
+++ elsa_query.sh
@@ -9,4 +9,4 @@ HASH=$(printf '%s' "$EPOCH$APIKEY" |shasum -a 512)
 HEADER=$(echo "Authorization: ApiKey $USER:$EPOCH:$HASH" | sed -e s'/\-//')
 
 QUERY=$1
-curl -XPOST -k -H "$HEADER" -F "permissions={ \"class_id\": { \"0\": 1 }, \"program_id\": { \"0\": 1 }, \"node_id\": { \"0\": 1 }, \"host_id\": { \"0\": 1 } }" -F "query_string=$QUERY" https://127.0.0.1:3154/API/query > $2
+curl -XPOST -k -H "$HEADER" -F "permissions={ \"class_id\": { \"0\": 1 }, \"program_id\": { \"0\": 1 }, \"node_id\": { \"0\": 1 }, \"host_id\": { \"0\": 1 } }" -F "query_string=$QUERY" https://127.0.0.1/elsa-query/API/query > $2
